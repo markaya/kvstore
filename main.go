ad4c6140c3199ab3b695a383ff0c809adf29e2f5
markaya <markoristic15@gmail.com>
2025-12-28 21:59:30 +0100

    add basic sstables, manifest and basic cache for upcoming lsm tree

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"cmp"
+	"container/list"
 	"context"
 	"crypto/tls"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -12,6 +17,7 @@ import (
 	"os/signal"
 	"runtime/debug"
 	"slices"
+	"strconv"
 	"strings"
 	"sync"
 	"syscall"
@@ -22,6 +28,13 @@ var SERVER_ADDRESS = ":4000"
 var TLSPATH = "/Users/markoristic/open-source/kvstore/tls"
 var DEBUG_MODE = true
 var LOG_REQUESTS = true
+
+var ErrWrongPercentileFormat = errors.New("wrong percentile format, please use number between 0 and 1")
+var ErrHistogramEmpty = errors.New("histogram does not have any data")
+var ErrNoRecord = errors.New("record does not exist in database")
+
+var storage = NewStorage()
+var histogram = NewLatencyHistogram([]int64{3000, 6000, 9000, 12000, 15000, 25000, 35000, 70000, 90000, 110000, 200000, 300_000, 600_000, 800_000, 1_000_000, 1_500_000, 2_000_000, 2_500_000, 3_000_000})
 
 type LatencyHistogram struct {
 	mu      sync.RWMutex
@@ -53,17 +66,18 @@ func (h *LatencyHistogram) Add(duration time.Duration) {
 	h.counts[len(h.counts)-1]++
 }
 
-// GetPercentile is a method from histogram that returns bounds for specified percentile p50;p90;p99
+// GetPercentile is a method from histogram that returns bounds for specified percentile e.g. p50,p90,p99;
+//
 // p is number between 0.0 and 1.0
-func (h *LatencyHistogram) GetPercentile(p float64) int64 {
+func (h *LatencyHistogram) GetPercentile(p float64) (int64, error) {
 	if p < 0 || p > 1 {
-		return 0
+		return 0, ErrWrongPercentileFormat
 	}
 	h.mu.Lock()
 	defer h.mu.Unlock()
 
 	if h.total == 0 {
-		return 0
+		return 0, ErrHistogramEmpty
 	}
 
 	targetCount := p * float64(h.total)
@@ -73,13 +87,221 @@ func (h *LatencyHistogram) GetPercentile(p float64) int64 {
 		currentCount += count
 		if float64(currentCount) >= targetCount {
 			if i >= len(h.bukcets) {
-				return h.bukcets[len(h.bukcets)-1]
+				return h.bukcets[len(h.bukcets)-1], nil
 
 			}
-			return h.bukcets[i]
+			return h.bukcets[i], nil
 		}
 	}
-	return 0
+	return 0, errors.New("something went wrong")
+}
+
+type SSTableEntry struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+type LruCache struct {
+	capacity int
+	items    map[string]*list.Element
+	queue    *list.List
+}
+
+func NewLruCache() *LruCache {
+	return &LruCache{
+		capacity: 500,
+		items:    make(map[string]*list.Element),
+		queue:    list.New(),
+	}
+}
+
+func (l *LruCache) Get(key string) (string, bool) {
+	if element, ok := l.items[key]; ok {
+		l.queue.MoveToFront(element)
+		return element.Value.(*SSTableEntry).Value, true
+	}
+	return "", false
+}
+
+func (l *LruCache) Put(entry SSTableEntry) {
+	if element, ok := l.items[entry.Key]; ok {
+		l.queue.MoveToFront(element)
+		element.Value.(*SSTableEntry).Value = entry.Value
+		return
+	}
+	if l.queue.Len() >= l.capacity {
+		oldest := l.queue.Back()
+		if oldest != nil {
+			l.queue.Remove(oldest)
+			kv := oldest.Value.(*SSTableEntry)
+			delete(l.items, kv.Key)
+		}
+	}
+
+	element := l.queue.PushFront(&entry)
+	l.items[entry.Key] = element
+}
+
+type Storage struct {
+	memTableMu    sync.RWMutex
+	memTable      map[string]string
+	lruCache      *LruCache
+	sstIdx        int64
+	manifestCache []string
+}
+
+func NewStorage() *Storage {
+	memMap := make(map[string]string, 2100)
+
+	storage := Storage{memTable: memMap, lruCache: NewLruCache()}
+	file, err := os.OpenFile("./MANIFEST", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0644)
+	if err != nil {
+		panic(fmt.Sprintf("There was an error trying to open MANIFEST: %v", err))
+	}
+	defer file.Close()
+
+	var lastLine string
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		lastLine = scanner.Text()
+		storage.manifestCache = append(storage.manifestCache, lastLine)
+	}
+
+	if lastLine != "" {
+		temp := strings.Split(lastLine, "-")
+		idxStr := strings.TrimSuffix(temp[1], ".json")
+		idx, err := strconv.Atoi(idxStr)
+		if err != nil {
+			slog.Error(fmt.Sprintf("Error converting %s: %v\n", idxStr, err))
+			panic("error loading sstable id")
+		}
+		storage.sstIdx = int64(idx) + 1
+
+	}
+
+	return &storage
+
+}
+
+func (s *Storage) Shutdown() error {
+	return s.Flush(true)
+
+}
+
+func (s *Storage) Flush(isShuttingDown bool) error {
+	s.memTableMu.Lock()
+	defer s.memTableMu.Unlock()
+
+	if len(s.memTable) < 2000 && !isShuttingDown {
+		return errors.New("not ready to flush yet, need 2000 records for flush")
+	}
+
+	entries := make([]SSTableEntry, 0, len(s.memTable))
+	for k, v := range s.memTable {
+		entries = append(entries, SSTableEntry{Key: k, Value: v})
+	}
+
+	slices.SortFunc(entries, func(a, b SSTableEntry) int {
+		return cmp.Compare(a.Key, b.Key)
+	})
+
+	filename := fmt.Sprintf("./sstables/sst-%d.json", s.sstIdx)
+	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+	if err != nil {
+		slog.Error("Flush not successfull", "err", err.Error())
+		return fmt.Errorf("unsuccessfull flush: %w", err)
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(entries); err != nil {
+		slog.Error("could not encode data to file")
+
+		return fmt.Errorf("unsuccessfull flush: %w", err)
+	}
+
+	manifestFile, err := os.OpenFile("./MANIFEST", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0644)
+	if err != nil {
+		slog.Error(fmt.Sprintf("There was an error trying to open MANIFEST: %v", err))
+		panic("could not open MANIFEST file")
+	}
+	defer manifestFile.Close()
+
+	_, err = fmt.Fprint(manifestFile, filename, "\n")
+	if err != nil {
+		slog.Error("Flush not successfull, manifest write failed", "err", err.Error())
+		err := file.Close()
+		if err != nil {
+			slog.Error("Could not close file %s, before its removal", "file name", file.Name())
+			panic("Error when closing file before its removal, cant proceed")
+		}
+		err = os.Remove(filename)
+		if err != nil {
+			slog.Error("Failed to remove sstable file after failed manifest write. ShutdownDB")
+			panic("Panic:Failed to remove sstable file after failed manifest write. ShutdownDB")
+		}
+		return fmt.Errorf("unsuccessfull flush: %w", err)
+	}
+
+	// NOTE: Here I have this at this time until I get to sparse indexes.
+	storage.lruCache = NewLruCache()
+	storage.manifestCache = append(storage.manifestCache, filename)
+	clear(s.memTable)
+	s.sstIdx++
+	return nil
+}
+
+func (s *Storage) AddValue(key, value string) {
+	s.memTableMu.Lock()
+	s.memTable[key] = value
+	s.memTableMu.Unlock()
+
+	if len(s.memTable) >= 2000 {
+		_ = s.Flush(false)
+	}
+}
+
+func (s *Storage) RetrieveValue(key string) (string, error) {
+	s.memTableMu.RLock()
+	defer s.memTableMu.RUnlock()
+
+	value, ok := s.memTable[key]
+	if ok {
+		return value, nil
+	}
+
+	cacheValue, ok := s.lruCache.Get(key)
+	if ok {
+		return cacheValue, nil
+	}
+
+	return s.RetrieveFromSST(key)
+
+}
+
+func (s *Storage) RetrieveFromSST(key string) (string, error) {
+	for i := len(s.manifestCache) - 1; i >= 0; i-- {
+		filename := s.manifestCache[i]
+		bytes, err := os.ReadFile(filename)
+		if err != nil {
+			return "", err
+		}
+
+		var entries []SSTableEntry
+		if err := json.Unmarshal(bytes, &entries); err != nil {
+			return "", err
+		}
+
+		for _, v := range entries {
+			if v.Key == key {
+				s.lruCache.Put(v)
+				return v.Value, nil
+			}
+		}
+	}
+	return "", ErrNoRecord
 }
 
 func main() {
@@ -113,7 +335,12 @@ func main() {
 		}
 	}()
 
+	slog.Info("Waiting for shutdown signal...")
 	<-ctx.Done()
+	err := storage.Shutdown()
+	if err != nil {
+		slog.Error("There was an error when shutting down storage", slog.String("err", err.Error()))
+	}
 	slog.Info("shutdown signal received; gracefully shutting down server.")
 
 }
@@ -130,14 +357,10 @@ func registerRoutes(mux *http.ServeMux, logRequestFlag bool) http.Handler {
 	return metricsMiddleware(recoverPanic(mux))
 }
 
-var kv_store = make(map[string]string)
-
-var histogram = NewLatencyHistogram([]int64{3000, 6000, 9000, 12000, 15000, 25000, 35000, 70000})
-
 func showHistogram(w http.ResponseWriter, r *http.Request) {
-	p50 := histogram.GetPercentile(0.5)
-	p90 := histogram.GetPercentile(0.9)
-	p99 := histogram.GetPercentile(0.99)
+	p50, _ := histogram.GetPercentile(0.5)
+	p90, _ := histogram.GetPercentile(0.9)
+	p99, _ := histogram.GetPercentile(0.99)
 	slog.Info(fmt.Sprintf("Value for p50 is %d ns", p50))
 	slog.Info(fmt.Sprintf("Value for p90 is %d ns", p90))
 	slog.Info(fmt.Sprintf("Value for p99 is %d ns", p99))
@@ -158,7 +381,7 @@ func putValue(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	bodyString := string(body)
-	kv_store[key] = bodyString
+	storage.AddValue(key, bodyString)
 	w.WriteHeader(200)
 }
 
@@ -169,14 +392,19 @@ func getValue(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	res, ok := kv_store[key]
-	if !ok {
-		w.WriteHeader(http.StatusNotFound)
+	res, err := storage.RetrieveValue(key)
+	if err != nil {
+		if errors.Is(err, ErrNoRecord) {
+			w.WriteHeader(http.StatusNotFound)
+		} else {
+			slog.Error("error on retrieving value", slog.String("err", err.Error()))
+			w.WriteHeader(http.StatusInternalServerError)
+		}
 		return
 	}
 
 	w.WriteHeader(200)
-	_, err := fmt.Fprint(w, res)
+	_, err = fmt.Fprint(w, res)
 	if err != nil {
 		slog.Error("There was an error when writing a response", slog.String("err", err.Error()))
 	}
@@ -210,13 +438,13 @@ func logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		url := r.URL.RequestURI()
 		if !strings.Contains(url, "/static") && !strings.Contains(url, "/.well-known") {
-			slog.Info(
-				"Received Request",
-				slog.String("remmote_addr", r.RemoteAddr),
-				slog.String("proto", r.Proto),
-				slog.String("method", r.Method),
-				slog.String("uri", r.URL.RequestURI()),
-			)
+			// slog.Info(
+			// 	"Received Request",
+			// 	slog.String("remmote_addr", r.RemoteAddr),
+			// 	slog.String("proto", r.Proto),
+			// 	slog.String("method", r.Method),
+			// 	slog.String("uri", r.URL.RequestURI()),
+			// )
 		}
 		next.ServeHTTP(w, r)
 	})
